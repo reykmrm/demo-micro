d86b7a01f15ed133fee92391ef665540dc31babd
Reyk Mrm <8428349+reykmrm@users.noreply.github.com>
2022-08-22 18:07:22 -0500

    refactory

--- src/main/java/com/reykmrm/ControlCalculadora.java
+++ src/main/java/com/reykmrm/ControlCalculadora.java
@@ -1,15 +1,41 @@
 package com.reykmrm;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControlCalculadora {
-	@GetMapping("/suma/{a}/{b}")
-	public Respuesta sumarNumero(@PathVariable int a, @PathVariable int b) {
-		Respuesta rs = new Respuesta(a, b, a+b);
-		return rs;
+	@Value("${app.message.error_division_cero}")
+	private String mensajeErrorDivCero;
+
+	@GetMapping("/sumar/{a}/{b}")
+	public Respuesta sumar(@PathVariable double a, @PathVariable double b) {
+		Respuesta res = new Respuesta();
+		res.setA(a);
+		res.setB(b);
+		res.setResultado(a + b);
+		return res;
+	}
+
+	@GetMapping("/restar/{a}/{b}")
+	public Respuesta restar(@PathVariable double a, @PathVariable double b) {
+		Respuesta res = new Respuesta(a, b, (a - b));
+		return res;
+	}
+
+	@GetMapping("/multiplicar/{a}/{b}")
+	public Respuesta multiplicar(@PathVariable double a, @PathVariable double b) {
+		return new Respuesta(a, b, (a * b));
+	}
+
+	@GetMapping("/dividir/{a}/{b}")
+	public Respuesta dividir(@PathVariable double a, @PathVariable double b) {
+		if (b == 0) {
+			return new Respuesta(a, b, (a / b), mensajeErrorDivCero);
+		}
+		return new Respuesta(a, b, (a / b));
 	}
 
 }
--- src/main/java/com/reykmrm/Respuesta.java
+++ src/main/java/com/reykmrm/Respuesta.java
@@ -4,41 +4,57 @@ package com.reykmrm;
 
 public class Respuesta {
 	
-	private int a;
-	private int b;
-	private int resultado;
-	
-	
-	public int getA() {
+	private double a;
+	private double b;
+	private double resultado;
+	private String error = "Ningún error";
+
+	public Respuesta() {
+	}
+
+	public Respuesta(double a, double b, double resultado) {
+		this.a = a;
+		this.b = b;
+		this.resultado = resultado;
+	}
+
+	public Respuesta(double a, double b, double resultado, String error) {
+		this.a = a;
+		this.b = b;
+		this.resultado = resultado;
+		this.error = error;
+	}
+
+	public double getA() {
 		return a;
 	}
-	
-	
-	public Respuesta(int a, int b, int resultado) {
-		super();
-		this.a = a;
-		this.b = b;
-		this.resultado = resultado;
-	}
 
-
-	public void setA(int a) {
+	public void setA(double a) {
 		this.a = a;
 	}
-	public int getB() {
+
+	public double getB() {
 		return b;
 	}
-	public void setB(int b) {
+
+	public void setB(double b) {
 		this.b = b;
 	}
-	public int getResultado() {
+
+	public double getResultado() {
 		return resultado;
 	}
-	public void setResultado(int resultado) {
+
+	public void setResultado(double resultado) {
 		this.resultado = resultado;
 	}
-	
-	
-	
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
 
 }
